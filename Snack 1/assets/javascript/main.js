73ba9bc3ce6c7684ed1acb778944b2164e56f26c
yurididomenico <yurididomenico93@gmail.com>
2022-10-26 15:47:02 +0200

    Snack 1: Check

--- Snack 1/assets/javascript/main.js
+++ Snack 1/assets/javascript/main.js
@@ -2,11 +2,26 @@
 
 console.log("Ciao!");
 
+/*
+*Snack1*
+-Crea un array vuoto
+-Chiedi all’utente un numero da inserire nell’array.
+-Chiedere i numeri all’utente
+-Inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+*/
 
+let v = [];
+let somma = 0;
+let i = 0;
 
+while(somma < 50)
+{
+    v[i] = parseInt(prompt('Valore: ')); console.log(v[i]);
+    somma += v[i];
+    i++;
+}
 
-
-
+console.log(`La somma è: ${somma}`);
 
 
 
