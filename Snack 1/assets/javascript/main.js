907c210b99eec18344f3d25a5808751f74af94ec
yurididomenico <yurididomenico93@gmail.com>
2022-10-26 16:39:11 +0200

    Snack 4: Check

--- Snack 1/assets/javascript/main.js
+++ Snack 1/assets/javascript/main.js
@@ -49,7 +49,7 @@ Genera N array,
 ognuno formato da 10 numeri casuali da 1 a 100.
 Ogni volta che ne crei uno, stampalo.
 */
-
+/* 
 let N = parseInt(prompt(`N° Vettori da stampare: `)); 
 let cont = 0;
 let array = []
@@ -64,6 +64,51 @@ while(cont < N)
     console.log(array);
     cont++;
 }
+*/
+
+/* 
+*Snack4 (Bonus)*
+In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby.
+-Chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+-Interrompi il ciclo appena il nome è stato trovato.
+*/
+
+let listaNomi = ["Giuseppino", "Alfonsetto", "Rodolfuccio"];
+let i=0;
+let flagOn = false;
+
+let nome = prompt(`Nome: `);
+
+while(i < listaNomi.length)
+{
+    if(nome != listaNomi[i])
+    {
+        flagOn = false;
+    }
+    else
+    {
+        flagOn = true;
+        console.log("test");
+        break;
+    }
+
+    i++;
+}
+
+
+if(flagOn == true)
+{
+    console.log(`Sei presente nella lista! :)`);
+}
+else
+{
+    console.log(`Mi dispiace ma non puoi entrare! :(`);
+}
+
+
+
+
+
 
 
 
